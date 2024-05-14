2f84a30b3c02e1cf3204e8784651cc8f6f6b6cbe
prateek <Kesharwani3899@gmail.com>
2024-05-15 02:02:08 +0530

    Add all

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input"
+import Button from "./Button"
+import Select from "./Select"
+import Login from "./Login";
+import Signup from "./Signup";
+import Protected from "./AuthLayout"
+import RTE from "./RTE";
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard"
+
+export {Header,Footer,Container,Logo,LogoutBtn,Button,Input,Select,Login,Signup,Protected,RTE,PostForm,PostCard} 
